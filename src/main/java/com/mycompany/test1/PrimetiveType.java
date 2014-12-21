91ca36a7adcd11a77ff5a2a6e528acb47de7d546
Ann <Ann@Anncpe12>
2014-12-21 11:03:29 +0700

    First commit

---
+++ src/main/java/com/mycompany/test1/PrimetiveType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.test1;
+
+/**
+ *
+ * @author Ann
+ */
+public class PrimetiveType {
+    
+    public static void main (String[] args) {
+        byte a = 127;
+    }
+    
+}
